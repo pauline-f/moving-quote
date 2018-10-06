83d8ac4c12746336de53b132a19df886050281c5
pauline <pauline.feytel@gmail.com>
2018-10-06 11:02:49 +0200

    Add component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CreateComponent } from './quote/create/create.component';
 import { ViewComponent } from './quote/view/view.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateComponent,
-    ViewComponent
+    ViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
