061a6a3de34a7b8d6eacd19a7bfc3bd5152a653d
pauline <pauline.feytel@gmail.com>
2018-10-06 11:30:01 +0200

    Create header.html and add Routes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { CreateComponent } from './quote/create/create.component';
 import { ViewComponent } from './quote/view/view.component';
 import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes = [
+  { path:'quote/create', component:CreateComponent },
+  { path:'quote/view', component:ViewComponent }
+]
 
 
 @NgModule({
@@ -16,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
