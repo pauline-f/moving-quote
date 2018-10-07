d2822ae0cbaf80d6f8f53e0e0390a6ae0cc432c2
pauline <pauline.feytel@gmail.com>
2018-10-07 15:51:38 +0200

    Add Quote.models and service/quotes

---
+++ src/app/models/Quote.model.ts
@@ -0,0 +1,5 @@
+export class Quote {
+    constructor(public name:string, public mail:string, public address:string, public distance:number, public surface:number,
+                public atticCellar:number, public piano:boolean, public packHelpful:boolean) {
+    }
+}
