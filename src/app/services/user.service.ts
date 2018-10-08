8b03df4cfa5006a11b42151d9077cb1d30f7688e
pauline <pauline.feytel@gmail.com>
2018-10-08 21:06:33 +0200

    Create user.service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class UserService {
 
   constructor() { }
+
+  createNewUser(mail:string, password:string) {
+    return new Promise (
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(mail, password).then (
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  loginUser(mail:string, password:string) {
+    return new Promise (
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(mail, password).then (
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOutUser() {
+    firebase.auth().signOut();
+  }
 
 }
